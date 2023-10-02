54f6a46233b1805c0c5c66cc914f8f184db314d5
tabares7 <pedrotabares777@gmail.com>
2023-10-02 14:25:35 -0500

    Completando la informacion en Telemedicina compone

--- labuenafeweb/src/components/StarredServices.jsx
+++ labuenafeweb/src/components/StarredServices.jsx
@@ -12,17 +12,17 @@ const StarredServices = () => {
       <div className="border-2 border-white rounded-lg p-5 flex flex-col gap-5">
         <GiMedicalDrip className="text-5xl"/>
         <h2 className="text-2xl font-poppins-600">Sueros Vitaminados</h2>
-        <p>Energia asegurada con altas dosis de vitaminas.</p>
+        <p>Ofrecemos sueros vitaminados diseñados para revitalizar y nutrir tu cuerpo desde el interior.</p>
       </div>
       <div className="border-2 border-white rounded-lg p-5 flex flex-col gap-5">
         <BsTruck className="text-5xl"/>
         <h2 className="text-2xl font-poppins-600">Examen DOT</h2>
-        <p>Energia asegurada con altas dosis de vitaminas.</p>
+        <p>Ofrecemos el examen DOT, una evaluación médica esencial para su licencia de conducir vehículos comerciales en los Estados Unidos.</p>
       </div>
       <div className="border-2 border-white rounded-lg p-5 flex flex-col gap-5">
         <LuMonitorSmartphone className="text-5xl"/>
         <h2 className="text-2xl font-poppins-600">Telemedicina</h2>
-        <p>Energia asegurada con altas dosis de vitaminas.</p>
+        <p>Estamos comprometidos con brindar atención accesible y de alta calidad, sin importar la distancia.</p>
       </div>
     </section>
   );
--- labuenafeweb/src/components/TeleHealth.jsx
+++ labuenafeweb/src/components/TeleHealth.jsx
@@ -16,9 +16,7 @@ const TeleHealth = () => {
           <h3 className="text-lg font-normal text-lightblue">Paso 1.</h3>
           <h2 className="text-2xl md:text-3xl text-darkblue font-poppins-800">Accede a la sala vitual</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur porro accusantium corrupti quas voluptatem soluta quam
-            vitae eaque ipsum omnis?
+          Accede a nuestra plataforma de telemedicina desde tu computadora, tablet o smartphone. Asegúrate de tener una conexión estable a internet y de permitir el acceso a tu cámara y micrófono para una comunicación fluida con nuestro equipo médico.
           </p>
         </div>
       </div>
@@ -28,9 +26,7 @@ const TeleHealth = () => {
           <h3 className="text-lg font-normal text-lightblue">Paso 2.</h3>
           <h2 className="text-2xl md:text-3xl text-darkblue font-poppins-800">Danos algunos datos</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur porro accusantium corrupti quas voluptatem soluta quam
-            vitae eaque ipsum omnis?
+          Una vez dentro, completa el formulario con tu información personal y médica. Esta información es esencial para que podamos ofrecerte una atención personalizada y adecuada a tus necesidades. Recuerda que todos tus datos están protegidos y se manejan con la máxima confidencialidad.
           </p>
         </div>
         <img src={info} alt="Imagen doctora toma notas" className="w-full lg:w-1/3" />
@@ -42,9 +38,7 @@ const TeleHealth = () => {
           <h3 className="text-lg font-normal text-lightblue">Paso 3.</h3>
           <h2 className="text-2xl md:text-3xl text-darkblue font-poppins-800">Recibe tu consulta</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur porro accusantium corrupti quas voluptatem soluta quam
-            vitae eaque ipsum omnis?
+          Una vez que hayas proporcionado tus datos, serás conectado con uno de nuestros profesionales de salud. Durante la consulta, podrás discutir tus síntomas, hacer preguntas y recibir recomendaciones. Al finalizar, si es necesario, recibirás una receta o indicaciones para un seguimiento.
           </p>
         </div>
       </div>
